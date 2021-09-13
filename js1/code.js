dec90c3d208746ef7276e7dc316969a86b483ca2
Maureen E <Maureenekeugo1@gmail.com>
2021-09-12 21:42:28 -0400

    Remove duplicates from list

---
+++ js1/code.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// Define a function that removes duplicates from an array of numbers and returns it as a result.
+// The order of the sequence has to stay the same.
+
+// SOlution:
+// function distinct(a) {
+// return a.filter((e, i) => {
+//     return a.indexOf(e) == i;
+// });
+// }
