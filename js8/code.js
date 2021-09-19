fc6ce827bec420aae2beff7be55ed05bc914914f
Maureen E <Maureenekeugo1@gmail.com>
2021-09-19 14:17:27 -0400

    in

---
+++ js8/code.js
@@ -0,0 +1,9 @@
+// Task:
+// Complete function saleHotdogs/SaleHotDogs/sale_hotdogs, function accept 1 parameters:n, n is the number of customers to buy hotdogs, different numbers have different prices (refer to the following table), return a number that the customer need to pay how much money.
+
+// SOLUTION:
+// function saleHotdogs(n){
+//   return n < 5 ? 100*n :
+//           n >= 5 && n < 10 ? 95*n : 90*n
+  
+// }
