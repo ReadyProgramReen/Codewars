177a6c5c1f7ccbfd4e4a0a93114c1a8633089bf9
Maureen E <Maureenekeugo1@gmail.com>
2021-09-20 14:21:27 -0400

    Alan Partridge II - Apple Turnover

---
+++ js9/code.js
@@ -0,0 +1,9 @@
+// Task
+// Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+// Note: Input will either be a positive integer (or a string for untyped languages).
+
+// SOLUTION:
+// function apple(x){
+//   let result = +x * +x 
+// return result >1000 ?'It\'s hotter than the sun!!':'Help yourself to a honeycomb Yorkie for the glovebox.'
+// }
