b4a3ded2db4cb54e1ade4869dc394463e09a62c9
Maureen E <Maureenekeugo1@gmail.com>
2021-09-25 20:37:24 -0400

    Square(n) Sum

---
+++ js00147/code.js
@@ -0,0 +1,17 @@
+// DESCRIPTION:
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+// SOLUTION 1:
+
+// function squareSum(numbers){
+//   let sum = 0
+// for(let i = 0 ; i<numbers.length;i++){
+//   sum = sum + Math.pow(numbers[i],2)
+// }
+//   return sum;
+// }
+// SOLUTION 2:
+
+// function squareSum(numbers){
+// return numbers.reduce((acc, e)=> acc + Math.pow(e,2),0)
+// }
