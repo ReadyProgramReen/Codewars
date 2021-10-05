825eb6c124d6149946d14577fb428bf7fb899f37
Maureen E <Maureenekeugo1@gmail.com>
2021-10-04 20:17:41 -0400

    Grasshopper - Variable Assignment Debug

---
+++ js23/code.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Variable assignment
+// Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+// SOLUTIONS:
+// var a = "dev"
+// var b = "Lab"
+// var name = a + b
