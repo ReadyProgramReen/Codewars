e981db3f48ec35653fa450553669685869f91464
Maureen E <Maureenekeugo1@gmail.com>
2021-10-05 20:27:15 -0400

    Welcome to the City

---
+++ js24/code.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// Create a method sayHello/say_hello/SayHello that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
+// Example:
+// sayHello(['John', 'Smith'], 'Phoenix', 'Arizona')
+// This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
+
+// SOLUTIONS:
+// function sayHello( name, city, state ) {
+//   return name.length > 1 ? `Hello, ${name.join(' ') }! Welcome to ${city}, ${state}!`: `Hello, ${name[0]}! Welcome to ${city}, ${state}!`
+// }
