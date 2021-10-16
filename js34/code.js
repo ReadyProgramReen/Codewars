fb5e9d8120849603a5446f76bd53b5aff8ed066f
Maureen E <Maureenekeugo1@gmail.com>
2021-10-15 22:44:39 -0400

    Find the position!

---
+++ js34/code.js
@@ -0,0 +1,11 @@
+// DESCRIPTION:
+// When provided with a letter, return its position in the alphabet.
+// Input :: "a"
+// Ouput :: "Position of alphabet: 1"
+
+// SOLUTIONS:
+// function position(letter){
+// //Write your own Code!
+//   let sol =((letter.charCodeAt(0)-97) + 1)
+//   return `Position of alphabet: ${sol}`
+// }
