a4dbbc2b9665448720227931c4c2e05fcd2ab5a5
Maureen E <Maureenekeugo1@gmail.com>
2021-11-03 11:23:59 -0400

    Exclamation marks series 2: Remove all exclamation marks from the end of sentence

---
+++ js53/code.js
@@ -0,0 +1,23 @@
+// DESCRIPTION:
+// Description:
+// Remove all exclamation marks from the end of sentence.
+// Examples
+// remove("Hi!") === "Hi"
+// remove("Hi!!!") === "Hi"
+// remove("!Hi") === "!Hi"
+// remove("!Hi!") === "!Hi"
+// remove("Hi! Hi!") === "Hi! Hi"
+// remove("Hi") === "Hi"
+
+// SOLUTION:
+// function remove (s) {
+// //  let Str = s.split('').reverse() 
+// //     let finding =Str.indexOf("i") !== -1 ? Str.indexOf("i"):Str.indexOf("y")!== -1 ?Str.indexOf("y"):Str.indexOf("a")
+// //     return Str.slice(finding).reverse().join('')
+// //  return s.endsWith('!') ? s.slice(0, -1) : s;
+  
+//   while (s && s.slice(-1) == '!') {
+//   s = s.slice(0,-1);
+//   }
+//   return s;
+// }
