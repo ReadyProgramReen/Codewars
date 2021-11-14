f2805e4484f3b0892ad7e2fd3cc31773323f1505
Maureen E <Maureenekeugo1@gmail.com>
2021-11-14 10:53:30 -0500

    sentence from left to right

---
+++ js64/code.js
@@ -0,0 +1,27 @@
+// DESCRIPTION:
+// Description:
+// Remove n exclamation marks in the sentence from left to right. n is positive integer.
+// Examples
+// remove("Hi!",1) === "Hi"
+// remove("Hi!",100) === "Hi"
+// remove("Hi!!!",1) === "Hi!!"
+// remove("Hi!!!",100) === "Hi"
+// remove("!Hi",1) === "Hi"
+// remove("!Hi!",1) === "Hi!"
+// remove("!Hi!",100) === "Hi"
+// remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
+// remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
+// remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
+// remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
+
+// SOLUTION:
+// function remove(s,n){
+//   //coding and coding....
+//   let input = s;
+//   while (n) {
+//   input = input.replace(/!/, "");
+//   n-=1;
+//   }
+//   return input;
+  
+// }
