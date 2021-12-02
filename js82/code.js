1ca87ac64e9651e611fc96693536250a94a48970
Maureen E <Maureenekeugo1@gmail.com>
2021-12-02 12:32:33 -0500

    loop statement --for

---
+++ js82/code.js
@@ -0,0 +1,25 @@
+// DESCRIPTION:
+// for loop is one of the most frequently used loop statements in JS. It works like while loop. In lesson 9, we wrote a while loop:
+// function sum1_100(){
+//   var sum=0,num=1
+//   while (num<=100){
+//     sum+=num;
+//     num++;
+//   }
+//   return sum;
+// }
+
+// Task
+// Coding in function pickIt, function accept 1 parameter:arr, it's a number array, we need traverse arr by using for loop, if element is odd number, push it to array odd, if it's a even number, push it to array even.
+// I've defined two array odd and even in the function, and also wrote the return statement. your work is write a for loop.
+// If you forgot how to push an element to array, please refer to lesson 4.
+// Series
+
+//  SOLUTION:
+
+// function pickIt(arr){
+//   var odd=[],even=[];
+//   //coding here
+//     for (let i = 0; i < arr.length; i++) {}
+//   return [arr.filter(number => number % 2 !== 0), arr.filter(number => number % 2 === 0)]
+// }
