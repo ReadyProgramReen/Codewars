cd0c903f26028b90b7e45edf5b50f04ec2f6a253
Maureen E <Maureenekeugo1@gmail.com>
2021-12-04 12:54:59 -0500

    Parse float

---
+++ js84/code.js
@@ -0,0 +1,7 @@
+// DESCRIPTION:
+// Write function parseF which takes an input and returns a number or null if conversion is not possible. The input can be one of many different types so be aware.
+
+//  SOLUTION:
+// function parseF(s) {
+//   return Number.isNaN(parseFloat(s))  ? null:parseFloat(s)
+// }
