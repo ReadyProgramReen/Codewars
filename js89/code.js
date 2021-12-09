c518cdf7a6aa260c3d8a1f10916d3bc7faac6337
Maureen E <Maureenekeugo1@gmail.com>
2021-12-09 13:17:03 -0500

    get ascii value of character

---
+++ js89/code.js
@@ -0,0 +1,9 @@
+// DESCRIPTION:
+// Get ASCII value of a character.
+// For the ASCII table you can refer to http://www.asciitable.com/
+
+// SOLUTION
+// function getASCII(c){
+//   return c.charCodeAt()
+//   // ...
+// }
