8b6d79510fcfd4127fcdee55f37bbb0c49a4468c
Maureen E <Maureenekeugo1@gmail.com>
2021-12-22 15:16:43 -0500

    Template Strings

---
+++ js102/code.js
@@ -0,0 +1,14 @@
+// DESCRIPTION:
+// Template Strings
+// Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+// Task
+// Your task is to return the correct string using the Template String Feature.
+// Input
+// Two Strings, no validation is needed.
+// Output
+
+// SOLUTION:
+
+// var TempleStrings = function(obj, feature) {
+//   return `${obj} are ${feature}`
+// }
