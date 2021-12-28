73c1f6f8f20787e9086e009aab148895914404fb
Maureen E <Maureenekeugo1@gmail.com>
2021-12-28 15:29:26 -0500

    UEFA EURO 2016

---
+++ js108/code.js
@@ -0,0 +1,12 @@
+// DESCRIPTION:
+// Finish the uefaEuro2016() function so it return string just like in the examples below:
+// uefaEuro2016(['Germany', 'Ukraine'],[2, 0]) // "At match Germany - Ukraine, Germany won!"
+// uefaEuro2016(['Belgium', 'Italy'],[0, 2]) // "At match Belgium - Italy, Italy won!"
+// uefaEuro2016(['Portugal', 'Iceland'],[1, 1]) // "At match Portugal - Iceland, teams playe
+
+// SOLUTION:
+// function uefaEuro2016(teams, scores){
+//   // your code...
+//   return scores[0] == scores[1] ? `At match ${teams[0]} - ${teams[1]}, teams played draw.`: scores[0] >scores[1]? `At match ${teams[0]} - ${teams[1]}, ${teams[0]} won!`:
+//                               `At match ${teams[0]} - ${teams[1]}, ${teams[1]} won!`
+// }
