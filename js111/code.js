ec3892ca5009abd88c44dd98cedb6ccc660a8414
Maureen E <Maureenekeugo1@gmail.com>
2022-01-01 15:37:15 -0500

    Ensure question

---
+++ js111/code.js
@@ -0,0 +1,13 @@
+// DESCRIPTION:
+// Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
+// For example (Input --> Output)
+// "Yes" --> "Yes?" 
+// "No?" --> "No?"
+
+// SOLUTION:
+
+// function ensureQuestion(s) {
+//   // Code here
+//   return s.endsWith('?')? s :`${s}?`
+// }
+
