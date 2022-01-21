ee80f5159a61fa670adb4b40572a42ecb69ed53b
Maureen E <Maureenekeugo1@gmail.com>
2022-01-21 18:10:10 -0500

    Create the rooms

---
+++ js131/code.js
@@ -0,0 +1,37 @@
+// DESCRIPTION:
+// Escape the room
+// You are creating an "Escape the room" game. The first step is to create a hash table called rooms that contains all of the rooms of the game. There should be at least 3 rooms inside it, each being a hash table with at least three properties (e.g. name, description, completed).
+// SOLUTION 1:
+// // Add rooms here
+// const rooms = {
+//   living :{
+//     name:"living",
+//     description:"big",
+//     completed:false,
+    
+//   },
+//   bathroom:{
+//     name:"bathroom",
+//     description:"small",
+//     completed:true,
+    
+//   },
+// Bedroom :{
+//     name:"Bedroom",
+//     description:"meduim",
+//     completed:"yes",    
+//   }
+// }
+// OR SOLUTION 2
+// class Room  {
+//   constructor() {
+//     this.name = '';
+//     this.description = '';
+//     this.completed= '';
+//   }
+// }
+// var rooms = {
+//   bedroom : new Room(),
+//   kitchen : new Room(),
+//   bathroom : new Room(),
+// }
