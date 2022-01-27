9808e67db8b711c3156a798c29e000f56f221893
Maureen E <Maureenekeugo1@gmail.com>
2022-01-27 18:28:25 -0500

    Pythagorean Triple

---
+++ js137/code.js
@@ -0,0 +1,19 @@
+// DESCRIPTION:
+// Given an array of 3 non-negative integers a, b and c, determine if they form a pythagorean triple.
+// A pythagorean triple is formed when:
+// c2 = a2 + b2
+// where c is the largest value of a, b, c.
+// For example: a = 3, b = 4, c = 5 forms a pythagorean triple, because 52 = 32 + 42
+// Return Values
+// 1 if a, b and c form a pythagorean triple
+// 0 if a, b and c do not form a pythagorean triple
+// For Python: return True or False
+// For JavaScript: return true or false
+// SOLUTION:
+// function isPythagoreanTriple(int) {
+//   // Good luck friends!
+//  let int2 = int.sort((a, b) => a - b)
+//                 .map((e)=>Math.pow(e,2))
+               
+//  return int2[2] ==  int2[0] + int2[1]
+// }
