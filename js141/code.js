ca7ad9a9aef56f5ea8706023522105b58f6dd83b
Maureen E <Maureenekeugo1@gmail.com>
2022-01-31 18:36:51 -0500

    Age Range Compatibility Equation

---
+++ js141/code.js
@@ -0,0 +1,28 @@
+// DESCRIPTION:
+// Everybody knows the classic "half your age plus seven" dating rule that a lot of people follow (including myself). It's the 'recommended' age range in which to date someone.
+
+// minimum age <= your age <= maximum age #Task
+// Given an integer (1 <= n <= 100) representing a person's age, return their minimum and maximum age range.
+// This equation doesn't work when the age <= 14, so use this equation instead:
+// min = age - 0.10 * age
+// max = age + 0.10 * age
+
+// You should floor all your answers so that an integer is given instead of a float (which doesn't represent age). Return your answer in the form [min]-[max]
+// ##Examples:
+// age = 27   =>   20-40
+// age = 5    =>   4-5
+// age = 17   =>   15-20
+
+// SOLUTION:
+// function datingRange(age){
+//   //return min-max
+//   let min = Math.floor(age/2 + 7)
+//   let max = Math.floor((age-7)*2)
+//   let min2 = Math.floor(age - 0.10 * age)
+//   let max2 = Math.floor(age + 0.10 * age)
+//   if(age > 14){
+//     return min + "-"+ max
+//   }else {
+//    return  min2 + "-"+  max2
+//   }
+// }
