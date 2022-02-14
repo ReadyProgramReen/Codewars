c688ffc32a66bd8a94e1dccdf55c0ed644b46686
Maureen E <Maureenekeugo1@gmail.com>
2022-02-13 22:53:24 -0500

    What's up next?

---
+++ js154/code.js
@@ -0,0 +1,28 @@
+// DESCRIPTION:
+// Given a sequence of items and a specific item in that sequence, return the item immediately following the item specified. If the item occurs more than once in a sequence, return the item after the first occurence. This should work for a sequence of any type.
+// When the item isn't present or nothing follows it, the function should return nil in Clojure and Elixir, Nothing in Haskell, undefined in JavaScript, None in Python.
+// nextItem([1, 2, 3, 4, 5, 6, 7], 3) # 4
+// nextItem("testing", "t") # "e"
+// SOLUTION:
+// function nextItem(a, item) {
+//   let found = false;
+
+//   for (let i of a) {
+//     if (i > (a.length || item + 1)) return;
+    
+//     if (found) {
+//       return i;
+//     }
+    
+//     if (i === item) {
+//       found = true;
+      
+//       if (a.next) {
+//         return a.next().value;
+//       }
+//     }
+//   }
+  
+//   console.log(a.length);
+//   return undefined;
+// }
