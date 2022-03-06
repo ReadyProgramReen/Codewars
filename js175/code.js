8d69b680e0f018b014b3176ce2c4a0b8b50227c3
Maureen E <Maureenekeugo1@gmail.com>
2022-03-06 15:24:00 -0500

    Total pressure calculation

---
+++ js175/code.js
@@ -0,0 +1,16 @@
+// DESCRIPTION:
+// Given the molecular mass of two molecules M1M_1M 
+// 1  and M2M_2M 2 , their masses present m1m_1m 1 and m2m_2m 2
+// ​ in a vessel of volume VVV at a specific temperature TTT, find the total pressure PtotalP_{total}P total exerted by the molecules. Formula to calculate the pressure is:
+
+// SOLUTION:
+
+// solution= (M1, M2, m1, m2, V, T) => {
+//   // your code goes here
+//    M1 = m1 * 0.001/M1;
+//     M2 = m2 * 0.001/M2;
+//     T = T + 273.15;
+//   var R = 0.082;
+
+//   return (((M1 + M2) * R * T) / V) * 1000;
+// }
