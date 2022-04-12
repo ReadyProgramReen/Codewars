6dc34cffd2a3a6b36edafca23d01678a0bda0a4b
Maureen E <Maureenekeugo1@gmail.com>
2022-04-11 20:56:34 -0400

    Even or Odd

---
+++ js208/code.js
@@ -0,0 +1,19 @@
+// DESCRIPTION:
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// SOLUTION:
+// function even_or_odd(number) {
+//   if(number % 2 === 0 ){
+//     return 'Even'
+//   }else{
+//     return 'Odd'
+//   }
+// }
+// SOLUTION2 :
+// function even_or_odd(number) {
+//   return number % 2 === 0 ? 'Even': 'Odd'
+// }
+//  SOLUTION3 :
+// function even_or_odd(number) {
+//    return number % 2 == 0 ?  'Even':  'Odd'
+// }
