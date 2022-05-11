bbda7ee0ec983178a2bfa5fb195353d5305aeb6e
Maureen E <Maureenekeugo1@gmail.com>
2022-05-10 22:23:15 -0400

    Codewars Solutions for You cant Code under pressure #1

---
+++ js0024/code.js
@@ -0,0 +1,11 @@
+
+// You Can't Code Under Pressure #1
+
+// Code as fast as you can! You need to double the integer and return it.
+
+// Solution :
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i *2;
+  }
