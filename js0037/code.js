20ca27a7f874e3d0ddee8c160e52b661fe305c58
Maureen E <Maureenekeugo1@gmail.com>
2022-06-07 01:58:38 -0400

    MakeUpperCase

---
+++ js0037/code.js
@@ -0,0 +1,25 @@
+// DESCRIPTION:
+
+// Write a function which converts the input string to uppercase.
+
+// Solution 1:
+// function makeUpperCase(str) {
+//     return str.toUpperCase()
+//    }
+
+//    Solution 2:
+
+//    function makeUpperCase(str) {
+//     // Code here
+//     return str.toUpperCase()
+//   }
+
+
+//    Solution 3:
+
+//    function makeUpperCase(str) {
+//     // Code here
+//     return str.toUpperCase()
+//   }
+
+   
