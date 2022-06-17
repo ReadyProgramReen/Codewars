dc13efa1f9b9b2879bd0dc51dd545d9a1236e9fa
Maureen E <Maureenekeugo1@gmail.com>
2022-06-17 19:58:07 -0400

    Reversed sequence

---
+++ js0047/code.js
@@ -0,0 +1,24 @@
+// DESCRIPTION:
+// Build a function that returns an array of integers from n to 1 where n>0.
+// Example : n=5 --> [5,4,3,2,1]
+
+
+// Solution 1 :
+
+// const reverseSeq = n => {
+// let n2 = [];
+// for(let i = 1;i<=n;i++){
+//   n2.unshift(i)
+// }
+//   return n2
+// };
+
+// Solution 2 :
+
+// const reverseSeq = n => {
+//  let array =[];
+//   for(let i = n; i>0 ; i--){
+//     array.push(i)
+//   }
+//   return array;
+// };
