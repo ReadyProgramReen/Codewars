de6a85327450c26b6415f9eb870b170d5c440bc5
Maureen E <Maureenekeugo1@gmail.com>
2022-07-02 21:32:31 -0400

    Grasshopper - Personalized Message

---
+++ js0062/code.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+// Use conditionals to return the proper message. 
+
+// Solution :
+// const greet = (name, owner) => {
+//  return  name == owner ? 'Hello boss':'Hello guest'
+// };
