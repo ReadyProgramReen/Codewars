4c03a3e664cf7fb6fd42ab557b3f51d66a2c8987
Maureen E <Maureenekeugo1@gmail.com>
2022-07-31 22:09:17 -0400

    squaring an argument

---
+++ js0091/code.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// SOLUTION:
+// // Write the "square"-function here
+// const square = n =>{
+//   return n*n
+// }
