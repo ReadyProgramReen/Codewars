4ec2f7d5a2fd12630e90e1d6fc2edfe18be79598
Maureen E <Maureenekeugo1@gmail.com>
2022-08-16 23:06:23 -0400

    Is it a palindrome?

---
+++ js00107/code.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// Write a function that checks if a given string (case insensitive) is a palindrome.
+
+// SOLUTION :
+// function isPalindrome(x) {
+//   // your code here
+  
+//  return x.toLowerCase() === x.split("").reverse().join("").toLowerCase()
+
+// }
