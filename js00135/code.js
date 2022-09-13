ba377cc3bb40cd0f3978229ec45709d9bec8a095
Maureen E <Maureenekeugo1@gmail.com>
2022-09-13 00:40:31 -0400

    101 Dalmatians - squash the bugs, not the dogs

---
+++ js00135/code.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// Your friend has been out shopping for puppies (what a time to be alive!)... He arrives back with multiple dogs, and you simply do not know how to respond!
+// By repairing the function provided, you will find out exactly how you should respond, depending on the number of dogs he has.
+// The number of dogs will always be a number and there will always be at least 1 dog.
+// SOLUTION:
+// function howManyDalmatians(n){
+  
+//   let r = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"]
+//   return n <= 10 ? r[0]:n<=50?r[1]:n<=100?r[2]:r[3]
+// }
