acf5e5060b1ba767fec3a9775a6a8e367cba6cd2
Maureen E <Maureenekeugo1@gmail.com>
2022-10-03 20:52:44 -0400

    Is this a triangle?

---
+++ js00154/code.js
@@ -0,0 +1,11 @@
+// DESCRIPTION:
+// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+// SOLUTION:
+
+// function isTriangle(a,b,c)
+// {
+//  let arr = [ a,b,c]
+//  let s = arr.sort((a,b)=>(a-b))
+//  return s[0] + s[1] > s[2]
+// }
