6fead794ff2a6fc7b67b7c7b45fb462da07c1967
Maureen E <Maureenekeugo1@gmail.com>
2022-10-09 21:01:22 -0400

    Sum of odd numbers

---
+++ js00160/code.js
@@ -0,0 +1,11 @@
+// DESCRIPTION:
+// Given the triangle of consecutive odd numbers:
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+// 1 -->  1
+// 2 --> 3 + 5 = 8
+
+// SOLUTION:
+// function rowSumOddNumbers(n) {
+//   // TODO
+// return Math.pow(n,3)
+// }
