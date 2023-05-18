66dafa8de7d03b9e87b194440d8906fc9fe611c4
Maureen E <maureenekeugo1@gmail.com>
2023-05-18 17:37:30 -0400

    Build string from a Hash

---
+++ js363.js
@@ -0,0 +1,16 @@
+// Building Strings From a Hash
+// Complete the solution so that it takes the object (JavaScript/CoffeeScript) or hash (ruby) passed in and generates a human readable string from its key/value pairs.
+
+// The format should be "KEY = VALUE". Each key/value pair should be separated by a comma except for the last pair.
+
+// Example:
+
+// solution({a: 1, b: '2'}) // should return "a = 1,b = 2"
+
+// Question:
+// function solution(pairs){
+//   // TODO: complete
+   
+
+// return Object.keys(pairs).map((v)=> `${v} = ${pairs[v]}`,).join(",")
+// }
