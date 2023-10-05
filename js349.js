e87248c1a280289727906df039e921a8cb28487d
Maureen E <maureenekeugo1@gmail.com>
2023-10-04 21:43:46 -0400

    explode repeats

---
+++ js349.js
@@ -0,0 +1,14 @@
+// Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+
+// Examples
+// "312" should return "333122"
+// "102269" should return "12222666666999999999"
+
+// Answer:
+
+// function explode(s) {
+// let newS = '';  //declare a variable to hold the new repeated string
+//   for(let i= 0; i<s.length;i++){ //to iterate each number 
+//     newS += s[i].repeat(+s[i]) // use the repeat method to repeat each number 
+//   }
+//   return newS  // return the newS variable with the repeated numbers.
