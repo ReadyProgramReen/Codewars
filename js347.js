a2565b43a8173b1360f9a9eb5033f0eb6df45305
Maureen E <maureenekeugo1@gmail.com>
2023-10-27 21:26:07 -0400

    grow

---
+++ js347.js
@@ -0,0 +1,9 @@
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+
+// Answer 1:
+// function grow(x){
+// return x.reduce((a,c)=>a*c,1)  // reduce method also works for multiplication but you have to change 0 to 1 at the end 
+// }
