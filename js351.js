26e866df76d948cc31c5c65f64a88070ba8cb89b
Maureen E <maureenekeugo1@gmail.com>
2023-11-05 22:29:02 -0500

    esrever

---
+++ js351.js
@@ -0,0 +1,8 @@
+// esreveR
+
+// Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
+
+// (the dedicated builtin(s) functionalities are deactivated)
+
+//Answer:
+// const reverse = (array) => array.reverse()  //used the reverse method 
