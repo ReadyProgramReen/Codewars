61ab9679ad15af1edc05c5c2e7415a6a6c4af662
Maureen E <maureenekeugo1@gmail.com>
2024-01-21 22:24:21 -0500

    spacify

---
+++ js539.js
@@ -0,0 +1,11 @@
+// Spacify:
+
+// Modify the spacify function so that it returns the given string with spaces inserted between each character.
+
+// spacify("hello world") // returns "h e l l o   w o r l d"
+
+// Answer :
+
+// function spacify(str) {
+//     return str.split('').join(' ')   //pretty simple . Im tired today .split=> into [] , and .join(' '), joined it with space for each character 
+//   }
