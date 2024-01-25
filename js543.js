559cd22dcaec58d74c593b718514c436b69453fc
Maureen E <maureenekeugo1@gmail.com>
2024-01-25 16:44:51 -0500

    flush

---
+++ js543.js
@@ -0,0 +1,25 @@
+
+// 1-Determine if the poker hand is flush
+
+// Determine if the poker hand is flush, meaning if the five cards are of the same suit.
+
+// Your function will be passed a list/array of 5 strings, each representing a poker card in the format "5H" (5 of hearts), meaning the value of the card followed by the initial of its suit (Hearts, Spades, Diamonds or Clubs). No jokers included.
+
+// Your function should return true if the hand is a flush, false otherwise.
+
+// The possible card values are 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A
+
+// Examples
+// ["AS", "3S", "9S", "KS", "4S"]  ==> true
+
+// ["AD", "4S", "7H", "KS", "10S"] ==> false
+
+// Answer :
+
+// function isFlush(cards) {
+  
+// return cards.every(a=>cards[0].slice(-1)===a.slice(-1))
+// }
+
+// // every() checks if every element in the array passes a specific test. so since we only
+// //care if( the last index of each element are equal to one another then .slice(-1) would be the best choice.
